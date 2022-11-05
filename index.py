9e1a6d826c72f6b9393c52ab88be825e9a72ea3b
Undefined727 <swalley2004@gmail.com>
2022-11-04 23:22:02 -0700

    owo

---
+++ index.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request
+import json
+from sqlalchemy import create_engine, MetaData, Column, Table, Integer, String
+
+
+
+app = Flask(__name__)
+students_engine = create_engine('sqlite:///students.db', echo = True)
+classes_engine = create_engine('sqlite:///classes.db', echo = True)
+students_meta = MetaData()
+classes_meta = MetaData()
+
+
+students = Table(
+   'students', students_meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('grade', Integer), 
+)
+classes = Table(
+   'students', classes_meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('grade', Integer), 
+)
+students_meta.create_all(students_engine)
+classes_meta.create_all(classes_engine)
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data/<student>', methods = ['GET', 'DELETE', 'PUT'])
+def studPull(student):
+    conn = engine.connect()
+    if(request.method == 'GET'):
+        data = json.loads('{"Filler": 85}')
+        del data["Filler"]
+        s = students.select().where(students.c.name == student)
+        result = conn.execute(s)
+        for row in result:
+            string = str(row)
+            got = '{"' + string[string.index(',')+3:string.index(',', string.index(',')+1)-1] + '": ' + string[string.index(',', string.index(',')+1)+2:string.index(')')] + '}'
+            got = json.loads(got)
+            data.update(got)
+        conn.close()
+        return data
+    if(request.method == 'DELETE'):
+        deleted = '{"' + student + '": "removed"}'
+        deleted = json.loads(deleted)
+        rem = students.delete().where(students.c.name == student)
+        conn.execute(rem)
+        conn.close()
+        return deleted
+    if(request.method == 'PUT'):
+        added = request.data
+        added = added.decode()
+        added = json.loads(added)
+        addedG = str(added['grade'])
+        rep = students.update().where(students.c.name == student).values(grade = addedG)
+        conn.execute(rep)
+        conn.close()
+        return added
+
+@app.route('/data', methods = ['GET', 'POST'])
+def genPull():
+    conn = engine.connect()
+    if(request.method == 'GET'):
+        data = json.loads('{"Filler": 85}')
+        del data["Filler"]
+        s = students.select()
+        result = conn.execute(s)
+        for row in result:
+            string = str(row)
+            got = '{"' + string[string.index(',')+3:string.index(',', string.index(',')+1)-1] + '": ' + string[string.index(',', string.index(',')+1)+2:string.index(')')] + '}'
+            got = json.loads(got)
+            data.update(got)
+        conn.close()
+        return data
+    if(request.method == 'POST'):
+        added = request.data
+        added = added.decode()
+        added = json.loads(added)
+        addedN = str(added['name'])
+        addedG = str(added['grade'])
+        added = '{"' + addedN + '": "' + addedG + '"}'
+        added = json.loads(added)
+
+        ins = students.insert().values(name = addedN, grade = addedG)
+        conn.execute(ins)
+        conn.close()
+        return added
+    
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/code.js
@@ -0,0 +1,57 @@
+let name = "null"
+let password = "null"
+
+async function addStudent() {
+        name = document.getElementById("student-name").value;
+        grade = document.getElementById("student-grade").value;
+
+        const response = await fetch('http://127.0.0.1:5000/data', {
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name, grade})})
+            const data = await response.text();
+            document.getElementById("gradebook").innerHTML = data;
+}
+
+async function editStudent() {
+    name = document.getElementById("student-name").value;
+    grade = Number(document.getElementById("student-grade").value);
+    name.replaceAll(' ', '%20');
+
+    const response = await fetch('http://127.0.0.1:5000/data/' + name, {
+        method: 'PUT', 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"grade": grade})})
+        const data = await response.text();
+        document.getElementById("gradebook").innerHTML = data;
+        search();
+}
+
+async function deleteStudent() {
+    name = document.getElementById("student-name").value;
+    name.replaceAll(' ', '%20');
+
+    const response = await fetch('http://127.0.0.1:5000/data/' + name, {method: 'DELETE',})
+        const data = await response.text();
+        document.getElementById("gradebook").innerHTML = data;
+}
+
+async function getStudents() {
+    const response = await fetch('http://127.0.0.1:5000/data', {method: 'GET', });
+    const data = await response.text();
+    document.getElementById("gradebook").innerHTML = data;
+}
+
+async function search() {
+    student = document.getElementById("student-name").value;
+    student.replaceAll(' ', '%20');
+    const response = await fetch('http://127.0.0.1:5000/data/' + student, {method: 'GET', });
+    const data = await response.text();
+    document.getElementById("listedStudent").innerHTML = data;
+}
