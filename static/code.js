831656122045e7465a645c170ee9385646dbe474
cfdoud <prouddoud01@gmail.com>
2022-11-13 21:03:50 -0800

    Christian-JS-hmtl-Change

--- static/code.js
+++ static/code.js
@@ -151,3 +151,21 @@ async function dropClass(studentID, classID) {
 async function getName() {
     return studentName;
 }
+document.getElementById(evt, "name").innerHTML = getName();
+
+function openCatalog(evt, tabChange)  {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i = 0; i < tabcontent.length; i++)  {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i = 0; i < tablinks.length; i++)  {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabChange).style.display = "block";
+    EventTarget.currentTarget.className += " active"
+}
